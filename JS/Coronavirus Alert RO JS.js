ad570bf7e7987ddb856ea6f21e78ff9513156689
SilviuGC <geo.curtui@gmail.com>
2020-11-16 15:49:41 +0200

    Added embeded google maps

---
+++ JS/Coronavirus Alert RO JS.js
@@ -0,0 +1,16 @@
+// Initialize and add the map
+function initMap() {
+  // The location of Uluru
+  const location_coordinates = { lat: 45.9442858, lng: 25.0094303 };
+  // The map, centered at Uluru
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 7.2,
+    center: location_coordinates,
+    gestureHandling: "none",
+  });
+  // The marker, positioned at Uluru
+  const marker = new google.maps.Marker({
+    position: location_coordinates,
+    map: map,
+  });
+}
