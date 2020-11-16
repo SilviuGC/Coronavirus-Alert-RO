17317a465fcf5f0d373b339adc2b1b2786dc5cb2
SilviuGC <geo.curtui@gmail.com>
2020-11-16 21:50:36 +0200

    BETA interface almost ready

--- JS/Coronavirus Alert RO JS.js
+++ JS/Coronavirus Alert RO JS.js
@@ -1,13 +1,14 @@
 // Initialize and add the map
 function initMap() {
   // The location of Uluru
-  const location_coordinates = { lat: 45.9442858, lng: 25.0094303 };
+  const location_coordinates = { lat: 45.9922858, lng: 25.0094303 };
   // The map, centered at Uluru
   const map = new google.maps.Map(document.getElementById("map"), {
     zoom: 7.25,
 	zoomControl: false,
     center: location_coordinates,
     gestureHandling: "none",
+	streetViewControl:false,
   });
   // The marker, positioned at Uluru
   const marker = new google.maps.Marker({
