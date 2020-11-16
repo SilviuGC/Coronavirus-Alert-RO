97c6d05b284b271590867c82c54ac9ad1300c399
SilviuGC <geo.curtui@gmail.com>
2020-11-16 18:54:40 +0200

    Added scrollbar to county menu

--- JS/Coronavirus Alert RO JS.js
+++ JS/Coronavirus Alert RO JS.js
@@ -4,7 +4,7 @@ function initMap() {
   const location_coordinates = { lat: 45.9442858, lng: 25.0094303 };
   // The map, centered at Uluru
   const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 7.2,
+    zoom: 7.25,
 	zoomControl: false,
     center: location_coordinates,
     gestureHandling: "none",
